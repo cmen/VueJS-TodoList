d9aa9adee7278eb6e42cbbc69f22ae4f54fb21ca
Christophe Meneses <christophe.meneses@gmail.com>
2018-05-29 13:18:39 +0200

    Import

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    {{ todos }}
+    <todos v-model="todos"></todos>
+    <todos v-model="todos"></todos>
+    <button @click="addTodo">Ajouter une tâche</button>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      todos: [{
+        name: 'Tâche 1',
+        completed: true
+      }]
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        name: 'Autre tâche',
+        completed: false
+      })
+    }
+  },
+  components: {
+    Todos
+  }
+}
+</script>
